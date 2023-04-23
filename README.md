145d0487b1eba1256de0c85c7b2777aab149d42f
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-04-23 19:38:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# YouTube Channel Subscription Tool
+
+This tool allows you to subscribe to a list of YouTube channels using the YouTube API v3. The list of channels should be provided as a CSV file with one channel URL per line. The tool handles authentication through OAuth 2.0, and it requires a client secrets JSON file to interact with the YouTube API.
+
+## Requirements
+
+- Python 3.6 or higher
+- Google API Python Client
+- Google Auth OAuthlib
+
+You can install the required packages using the following command:
+```
+pip install google-api-python-client google-auth-oauthlib
+```
+
+## Setup
+
+1. Create a new project in the [Google Developers Console](https://console.developers.google.com/).
+2. Enable the YouTube Data API v3 for your project.
+3. Create OAuth 2.0 credentials and download the client secrets JSON file.
+4. Rename the downloaded file to `client_secrets.json` and place it in the same directory as the script.
+5. Prepare a CSV file with the list of YouTube channel URLs you want to subscribe to. The CSV should have a header row with one of the columns labeled "URL", and each subsequent row should contain a YouTube channel URL in the "URL" column.
+
+## Usage
+```
+usage: python subscribe_tool.py [--secrets SECRETS] [--csv CSV]
+
+Subscribe to a list of YouTube channels from a CSV file.
+
+optional arguments:
+--secrets SECRETS Path to the client secrets JSON file (default: client_secrets.json)
+--csv CSV Path to the CSV file with channel URLs (default: channels.csv)
+```
+
+## Example
+
+To subscribe to the channels listed in a CSV file called `channels.csv` using the `client_secrets.json` file for authentication, run the following command:
+```python subscribe_tool.py --secrets client_secrets.json --csv channels.csv```
+
+
+## License
+
+This project is released under the MIT License.
---
+++ subscribe.py
@@ -0,0 +1,84 @@
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+import re
+import csv
+import argparse
+
+def get_channel_id(youtube, url):
+    """
+    Extracts the channel ID from a given YouTube URL.
+
+    Args:
+        youtube: A YouTube API client instance.
+        url (str): The YouTube channel URL.
+
+    Returns:
+        str: The channel ID if found, None otherwise.
+    """
+    patterns = [
+        r"(?<=channel/)[a-zA-Z0-9_-]+",  # Channel URL
+        r"(?<=user/)[a-zA-Z0-9_-]+"      # User URL
+    ]
+
+    for pattern in patterns:
+        channel_id = re.search(pattern, url)
+        if channel_id:
+            return channel_id.group(0)
+
+    return None
+
+def subscribe_to_channels(credentials, channel_urls):
+    youtube = googleapiclient.discovery.build("youtube", "v3", credentials=credentials)
+
+    for url in channel_urls:
+        channel_id = get_channel_id(youtube, url)
+        if channel_id:
+            try:
+                youtube.subscriptions().insert(
+                    part="snippet",
+                    body={
+                        "snippet": {
+                            "resourceId": {
+                                "kind": "youtube#channel",
+                                "channelId": channel_id
+                            }
+                        }
+                    }
+                ).execute()
+                print(f"Subscribed to channel with ID: {channel_id}")
+            except googleapiclient.errors.HttpError as error:
+                print(f"An error occurred: {error}")
+                print("Could not subscribe to the channel.")
+        else:
+            print(f"Invalid URL: {url}")
+
+def read_urls_from_csv(file_path):
+    channel_urls = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)  # Skip the header
+        for row in reader:
+            channel_urls.append(row[1])  # URLs are in the second column
+
+    return channel_urls
+
+def get_oauth_credentials(client_secrets_file, scopes):
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_local_server(port=0)
+    return credentials
+
+def main(client_secrets_file, csv_file_path):
+    scopes = ["https://www.googleapis.com/auth/youtube.force-ssl", "https://www.googleapis.com/auth/youtube"]
+    credentials = get_oauth_credentials(client_secrets_file, scopes)
+    channel_urls = read_urls_from_csv(csv_file_path)
+    subscribe_to_channels(credentials, channel_urls)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Subscribe to a list of YouTube channels from a CSV file.")
+    parser.add_argument("--secrets", help="Path to the client secrets JSON file", default="client_secrets.json")
+    parser.add_argument("--csv", help="Path to the CSV file with channel URLs", default="channels.csv")
+    args = parser.parse_args()
+
+    main(args.secrets, args.csv)
